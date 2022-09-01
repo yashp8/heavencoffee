23bff6ba3c3e40c31953e8e5aedbd8cee82ad16d
mowglie <Yash.Patel@yewtechnologies.co.uk>
2022-09-01 20:27:30 +0100

    static website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'heavencoffee';
+  menu: any;
+  navbar: any;
+  listener: any;
+
+  constructor(private renderer2: Renderer2) {
+    this.listener = this.renderer2.listen('window', 'scroll', (e) => {
+      if (this.menu) {
+        this.menu.classList.remove('fa-times');
+        this.navbar.classList.remove('active');
+      }
+
+    });
+  }
+
+  onNavclick() {
+    this.menu = <HTMLElement>document.querySelector('#menu-btn');
+    this.navbar = <HTMLElement>document.querySelector('.header .flex .navbar');
+    this.menu.classList.toggle('fa-times');
+    this.navbar.classList.toggle('active');
+  }
+
+
 }
